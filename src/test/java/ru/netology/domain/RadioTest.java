a8eac7c3fb8f738a46ff65d46807c9f3d220e039
Polina Moiseenko <trylyalya@rambler.ru>
2023-10-29 18:06:14 +0700

    Radio and RadioTest changed

---
+++ src/test/java/ru/netology/domain/RadioTest.java
@@ -0,0 +1,103 @@
+package ru.netology.domain;
+
+import org.junit.Test;
+import org.junit.jupiter.api.Assertions;
+
+import static org.junit.jupiter.api.Assertions.assertNull;
+
+public class RadioTest {
+    Radio radio = new Radio();
+
+    @Test
+    public void createRadio() {
+        String expected = "Uzer";
+        radio.setName(expected);
+        Assertions.assertEquals(expected, radio.getName());
+    }
+
+
+    @Test
+    public void validateChangeFields() {
+
+        radio.setCurrentRadioStation(4);
+        Assertions.assertEquals(4, radio.getCurrentRadioStation());
+    }
+
+
+
+    // тестируем станции
+    @Test
+    public void changeOverLastRadioStation() {
+        radio.setCurrentRadioStation(9);
+        radio.pressNextStation();
+        Assertions.assertEquals(0, radio.getCurrentRadioStation());
+    }
+
+
+    @Test
+    public void changeUnderInitialRadioStation() {
+        radio.setCurrentRadioStation(0);
+        radio.pressPrevStation();
+        Assertions.assertEquals(9, radio.getCurrentRadioStation());
+
+    }
+
+    @Test
+    public void nextRadioStation() {
+        radio.setCurrentRadioStation(6);
+        radio.pressNextStation();
+        Assertions.assertEquals(7, radio.getCurrentRadioStation());
+    }
+
+    @Test
+    public void prevRadioStation() {
+        radio.setCurrentRadioStation(6);
+        radio.pressPrevStation();
+        Assertions.assertEquals(5, radio.getCurrentRadioStation());
+    }
+
+    @Test
+    public void OverInitialRadioStation() {
+        radio.setCurrentRadioStation(11);
+        radio.getMaxRadioStation();
+        Assertions.assertEquals(0, radio.getCurrentRadioStation());
+    }
+
+    @Test
+    public void UnderInitialRadioStation() {
+        radio.setCurrentRadioStation(-1);
+        radio.getMinRadioStation();
+        Assertions.assertEquals(0, radio.getCurrentRadioStation());
+    }
+
+    // тестируем громкость
+    @Test
+    public void volumeOverMax() {
+        radio.setCurrentVolume(11);
+        radio.getMaxVolume();
+        Assertions.assertEquals(10, radio.getMaxVolume());
+    }
+
+    @Test
+    public void volumeDownUnderMin() {
+        radio.setCurrentVolume(-11);
+        radio.getMinVolume();
+        Assertions.assertEquals(0, radio.getMinVolume());
+
+    }
+
+    @Test
+    public void plusVolume() {
+        radio.setCurrentVolume(4);
+        radio.pressPlusVolume();
+        Assertions.assertEquals(5, radio.getCurrentVolume());
+    }
+
+    @Test
+    public void minusVolume() {
+        radio.setCurrentVolume(4);
+        radio.pressMinusVolume();
+        Assertions.assertEquals(3, radio.getCurrentVolume());
+    }
+}
+
